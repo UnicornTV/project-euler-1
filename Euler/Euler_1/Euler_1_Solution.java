4b7ef9b00832b5c9f44904bb804ce6d432477050
Oscar Villavicencio <odvcencio@gmail.com>
2016-11-07 05:31:39 -0500

    java solutions being added

---
+++ Euler/Euler_1/Euler_1_Solution.java
@@ -0,0 +1,16 @@
+//counter = 0
+//(0...1000).each { |i|
+//counter += i if i % 3 == 0 || i % 5 == 0 }
+
+class Euler_1_Solution{
+  public static void main(String[] args) {
+    int counter = 0;
+
+    for(int i = 0; i <= 1000; i++){
+      if(i % 3 == 0 || i % 5 == 0){
+        counter += i;
+      }
+    }
+    System.out.println(counter);
+  }
+}
