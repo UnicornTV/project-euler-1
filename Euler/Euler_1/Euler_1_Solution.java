6133ad00647a38b4151aff7cffc9d869ad4119bd
Oscar Villavicencio <odvcencio@gmail.com>
2016-11-07 05:33:57 -0500

    final edits to java solution

--- Euler/Euler_1/Euler_1_Solution.java
+++ Euler/Euler_1/Euler_1_Solution.java
@@ -5,8 +5,7 @@
 class Euler_1_Solution{
   public static void main(String[] args) {
     int counter = 0;
-
-    for(int i = 0; i <= 1000; i++){
+    for(int i = 3; i < 1000; i++){
       if(i % 3 == 0 || i % 5 == 0){
         counter += i;
       }
