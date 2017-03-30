a9498fe46b60db101ec5877769f559ea05d0c47a
Draco SkySong <oscar@unicorn.xxx>
2017-03-29 21:58:50 -0400

    euler 9 java

---
+++ Euler_9/Euler_9.java
@@ -0,0 +1,28 @@
+import java.math.*;
+
+public class Euler_9
+{
+    static int c = 0;
+
+    public static void main(String[] args) {
+      for(int a = 1; a<500; a++){
+        for(int b = a; b<500; b++){
+          Boolean check = isSquare(a*a + b*b);
+          if(check && a+b+c == 1000){
+            System.out.println(a*b*c);
+            break;
+          }
+        }
+      }
+    }
+
+    public static Boolean isSquare(int n){
+      int root = (int)Math.sqrt(n);
+      if (root * root == n){
+        c = root;
+        return true;
+      }
+      return false;
+    }
+
+}
