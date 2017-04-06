8e65c3a87691a9a59b88b75d8b01278615ca4afd
Draco SkySong <oscar@unicorn.xxx>
2017-04-06 00:39:54 -0400

    javal solution for Euler 12

---
+++ Euler_012/Euler_012.java
@@ -0,0 +1,31 @@
+import java.math.*;
+
+public class Euler_012{
+  public static void main(String[] args) {
+    long begin = System.currentTimeMillis();
+    for(int i = 0; true; i++){
+      if(findFactors(findTriangle(i)) > 500){
+          System.out.println(findTriangle(i));
+          break;
+      }
+    }
+    long end = System.currentTimeMillis();
+		System.out.println(end-begin + "ms");
+  }
+
+  public static int findTriangle(int term){
+    //how to compute a triangle number: triangle = (n * n+1)/2
+    return term = term * (term+1) / 2;
+  }
+  
+  public static long findFactors(long factored){
+    long factors = 0;
+    //test against the square root to check for perfect squares
+    int limit = (int)Math.sqrt(factored);
+    //ternary operator to check for primes
+    for(int i = 1; i<=limit; i++){
+      if (factored % i == 0){ factors += (factored % i == i ?  1 : 2); }
+    }
+    return factors;
+  }
+}
