1ed7b8575ef6dd5aff131324cd155a4791c77b55
Draco SkySong <oscar@unicorn.xxx>
2017-04-08 02:28:57 -0400

    Final changes for Euler 15

--- Euler_015/Euler_015.java
+++ Euler_015/Euler_015.java
@@ -1,15 +1,30 @@
+import java.math.*;
+
 class Euler_015{
   public static void main(String[] args) {
-    int latticePathCount = 0;
+    BigInteger latticePathCount = BigInteger.ZERO;
+    BigInteger _2n = BigInteger.ZERO;
+    BigInteger _n2 = BigInteger.ZERO;
 
-    latticePathCount = (int)factorial(2*20)/(int)(factorial(20)*factorial(20));
+    _2n = factorial(40);
+    _n2 = factorial(20);
+    _n2 = _n2.multiply(_n2);
+
+    latticePathCount = _2n.divide(_n2);
+    System.out.println(_2n);
+    System.out.println(_n2);
+    System.out.println(latticePathCount);
 
   }
-  static double factorial(int n)
+  static BigInteger factorial(int n)
   {
-    double result = 1;
-    for (double i = 1; i <= n; i++)
-        result *= i;
+    BigInteger result = BigInteger.ONE;
+    BigInteger index = BigInteger.ZERO;
+
+    for (int i = 1; i <= n; i++){
+        index = new BigInteger(Integer.toString(i));
+        result = result.multiply(index);
+    }
     return result;
   }
 }
